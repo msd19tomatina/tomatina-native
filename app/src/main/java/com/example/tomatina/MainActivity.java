9bcc593029d154ac7d160c5b7313ac5a2bc325db
Isi <islamoq@gmail.com>
2021-01-22 12:24:04 +0100

    add running Timer in Activity Main

--- app/src/main/java/com/example/tomatina/MainActivity.java
+++ app/src/main/java/com/example/tomatina/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tomatina;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,7 +13,29 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final long START_TIME_IN_MILLIS = 1500000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+    public MainActivity() {
+    }
+
+    public MainActivity(TextView mTextViewCountDown, Button mButtonStartPause) {
+        this.mTextViewCountDown = mTextViewCountDown;
+        this.mButtonStartPause = mButtonStartPause;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +50,61 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+
+
+        mButtonStartPause.setOnClickListener(v -> {
+            if (mTimerRunning) {
+                pauseTimer();
+            } else {
+                startTimer();
+            }
+        });
     }
 
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("Stop");
+
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        mButtonStartPause.setText("Neu beginnen");
+
+          }
+
+
+    public void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) mTimeLeftInMillis / 1000 % 60;
+
+        // Convert to a Time String --
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+
+    }
 }
+
