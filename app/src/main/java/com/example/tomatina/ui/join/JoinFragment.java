0386e5a4fe95034ef9118f971db5cacb9441cd8d
Isi <islamoq@gmail.com>
2021-01-24 17:52:46 +0100

    Navigation from Join to Timer

--- app/src/main/java/com/example/tomatina/ui/join/JoinFragment.java
+++ app/src/main/java/com/example/tomatina/ui/join/JoinFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.tomatina.R;
 
@@ -20,10 +25,19 @@ public class JoinFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         joinViewModel =
                 new ViewModelProvider(this).get(JoinViewModel.class);
         View root = inflater.inflate(R.layout.fragment_join, container, false);
 
+        ImageButton ib = (ImageButton) root.findViewById(R.id.button);
+        ib.setOnClickListener(new View.OnClickListener() {
+                                  @Override
+                                  public void onClick(View v) {
+                                      Navigation.findNavController(root).navigate(R.id.navigation_timer);
+                                  }
+                              }
+        );
         return root;
     }
 }
